77f554c18d6cdccf688bf86ff7ad243d9fe855c3
Jason Yoo <architectophile@gmail.com>
2024-01-11 14:21:42 +0900

    feat: add timeout

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
-import { ValidationPipe } from '@nestjs/common';
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-
+  const server = app.getHttpServer();
+  server.setTimeout(30 * 60 * 1000);
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
