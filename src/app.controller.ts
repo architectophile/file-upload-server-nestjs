43deb5b467c741154f052aefbbf6d962139869fd
architectophile <architectophile@gmail.com>
2024-01-14 19:38:08 +0900

    feat: return file as response

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,15 +4,18 @@ import {
   Get,
   ParseFilePipeBuilder,
   Post,
+  Res,
+  StreamableFile,
   UploadedFile,
   UseInterceptors,
 } from "@nestjs/common";
 import { FileInterceptor } from "@nestjs/platform-express";
-import { Express } from "express";
+import { Express, Response } from "express";
 import { AppService } from "./app.service";
 import { SampleDto } from "./sample.dto";
 import { diskStorage } from "multer";
 import { join } from "path";
+import { createReadStream } from "fs";
 
 @Controller()
 export class AppController {
@@ -36,11 +39,22 @@ export class AppController {
       }),
     })
   )
-  uploadData(@UploadedFile() file: Express.Multer.File) {
-    console.log("file: ", file);
-    return {
-      file: file.originalname,
-    };
+  uploadData(
+    @UploadedFile() file: Express.Multer.File,
+    @Res({ passthrough: true }) res: Response
+  ) {
+    console.log("uploaded file: ", file);
+
+    const fileStream = createReadStream(
+      join(process.cwd(), "downloads", file.originalname)
+    );
+
+    fileStream.on("error", (error) => {
+      console.error("Error while streaming file:", error);
+      res.status(500).send("Error while streaming file");
+    });
+
+    return new StreamableFile(fileStream);
   }
 
   @UseInterceptors(FileInterceptor("file"))
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,13 @@ import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Enable CORS for all routes and origins
+  app.enableCors();
+
   app.useGlobalPipes(new ValidationPipe());
   const server = app.getHttpServer();
-  server.setTimeout(30 * 60 * 1000);
+  server.setTimeout(30 * 60 * 1000); // Set timeout to 30 minutes
   await app.listen(3000);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
